173824e5f0bb61afa984bb2afc76c96b6dd1c33a
Thomas-Piroddi <thomas.piroddi45@gmail.com>
2021-01-18 14:27:41 +1100

    Publications and categories

--- client/src/components/categories.jsx
+++ client/src/components/categories.jsx
@@ -29,8 +29,23 @@ const Categories = () => {
 
   return (
     <div className="margin">
-      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('food')}>FOOD</button></Link>
+<Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('regional')}>REGIONAL</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('technology')}>TECHNOLOGY</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('lifestyle')}>LIFESTYLE</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('business')}>BUSINESS</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('general')}>GENERAL</button></Link>
       <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('programming')}>PROGRAMMING</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('science')}>SCIENCE</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('world')}>WORLD</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('sports')}>SPORTS</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('finance')}>FINANCE</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('academia')}>ACADEMIA</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('politics')}>POLITICS</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('health')}>HEALTH</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('opinion')}>OPINION</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('food')}>FOOD</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('game')}>GAME</button></Link>
+      <Link to={{pathname: '/topic'}} ><button onClick={handleClick} onMouseEnter={mouseEnter('food')}>FOOD</button></Link>
 
       <button>PublicationTEST</button>
       <button>Search</button>
